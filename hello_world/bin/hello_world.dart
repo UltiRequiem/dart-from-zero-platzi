6cd6506f71d487ce767cc2ac3c9cab311de1765b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-27 17:26:48 -0500

    :tada: Begin

---
+++ hello_world/bin/hello_world.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  print('Hello world');
+
+  final user = 23;
+
+  print('Hi user number $user!');
+
+  final friends = ['Bob', 'Tom', 'Alice'];
+
+  for (final friend in friends) {
+    print('Hello, $friend!');
+  }
+}
